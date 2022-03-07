6df753af69668300168639ab05f98ec3ea59de0a
Vivekanand15 <99167155+Vivekanand15@users.noreply.github.com>
2022-03-07 15:37:00 +0530

    Update App.js

--- sign-up/src/App.js
+++ sign-up/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import SignUp from "./SignUp";
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 
 
 const App=()=>{
