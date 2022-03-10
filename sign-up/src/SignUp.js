bb14980c6e39ce1828d49dab0b5ab3cc7cc24bfd
Vivekanand15 <99167155+Vivekanand15@users.noreply.github.com>
2022-03-10 11:54:12 +0530

    Update SignUp.js

--- sign-up/src/SignUp.js
+++ sign-up/src/SignUp.js
@@ -7,6 +7,54 @@ import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: {
+        translation: {
+          "Sign Up": "Sign Up",
+          "Sign up with your Gmail": "Sign up with your Gmail",
+          "First Name": "First Name",
+          "Last Name": "Last Name",
+          Email: "Email",
+          Password: "Password",
+          "Already have an account?": "Already have an account?",
+          "Sign In": "Sign In",
+        },
+      },
+      hi: {
+        translation: {
+          "Sign Up": "साइन अप करें",
+          "Sign up with your Gmail": "अपने जीमेल के साथ साइन अप करें",
+          "First Name": "पहला नाम",
+          "Last Name": "अंतिम नाम",
+          Email: "ईमेल",
+          Password: "पासवर्ड",
+          "Already have an account?": "पहले से ही एक खाता है?",
+          "Sign In": "साइन इन करें",
+        },
+      },
+    },
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+    detection: {
+      order: [
+        "querystring",
+        "cookie",
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+        "htmlTag",
+        "path",
+        "subdomain",
+      ],
+    },
+  });
 
 const theme = createTheme({
   typography:{
@@ -23,23 +71,23 @@ const SignUp = () => {
         <Grid container item xs={12} sm={6}>
           <Box width={400} sx={{mt: 2, mb: 2, ml: 10, mr: 2}}>
             <Typography variant="h3" sx={{ mt: 2, mb: 2, ml: 2, mr: 2 }}>
-              Sign Up
+              {t("Sign Up")}
             </Typography>
             <Typography variant="subtitle1" sx={{ mt: 2, mb: 5, ml: 2, mr: 2 }}>
-              Sign up with your Gmail
+             {t("Sign up with your Gmail")}
             </Typography>
             <Grid container spacing={2} sx={{ mr: 4 }}>
               <Grid item xs={6}>
-                <TextField label="First Name" />
+                <TextField label={t("First Name")} />
               </Grid>
               <Grid item xs={6}>
-                <TextField label="First Name" />
+                <TextField label={t("Last Name")} />
               </Grid>
               <Grid item xs={12}>
-                <TextField fullWidth required label="First Name" />
+                <TextField fullWidth required label={t("Email")} />
               </Grid>
               <Grid item xs={12}>
-                <TextField fullWidth required label="First Name" />
+                <TextField fullWidth required label={t("Password")} />
               </Grid>
               <Button
                 type="submit"
@@ -47,17 +95,17 @@ const SignUp = () => {
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Submit
+               {t("Sign Up")}
               </Button>
               <Grid item>
-                Already have an account?
+                 {t("Already have an account?")}
                 <Link
                   text-align="center"
                   underline="none"
                   href="#"
                   variant="body2"
                 >
-                   Sign In
+                  {t("Sign In")}
                 </Link>
               </Grid>
             </Grid>
