09eb6348d970881957f6cdd74a33215a3f7f14e6
Vivekanand15 <99167155+Vivekanand15@users.noreply.github.com>
2022-03-10 23:50:27 +0530

    Update App.js

--- sign-up/src/App.js
+++ sign-up/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import SignUp from "./SignUp";
-
-
-const App=()=>{
-    return (
-    <div>
-        <SignUp/>
-        
-    </div>
-    );
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+const theme = createTheme({
+  typography: {
+    fontFamily: "Inter",
+    body1: {
+      fontWeight: 700, // or 'bold'
+    },
+    button: {
+      textTransform: "none",
+    },
+  },
+});
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <SignUp />
+    </ThemeProvider>
+  );
 };
 export default App;
